9a404033c8175ecbd61996e98c040824cb42fedd
chris patrick <lovedknightt@yahoo.com>
2018-11-16 17:24:13 -0500

    environment variables

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
+const uName = process.env.USER;
+const uPass = process.env.PASS;
 
 const app = express();
 
 //allow cross origin requests
 app.use(cors());
+require("dotenv").config();
 //connected to the database and log when connection received.
-mongoose.connect("mongodb://cpat3:X22dPqCP!m@ds161856.mlab.com:61856/db-chrispcodes", { useNewUrlParser: true});
+mongoose.connect("mongodb://uName:uPass@ds161856.mlab.com:61856/db-chrispcodes", { useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('connected to database!');
 });
